ca6d86bcdd09471d6a2eb87b04dcf71306882159
Nikita Levchuk <code@levch.uk>
2024-02-27 20:53:43 +0100

    feature: add user greetings method to the Cli class

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+class Cli {
+    public static void greetings() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
