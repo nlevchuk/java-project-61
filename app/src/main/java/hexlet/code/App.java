6d8036843f2727c4015816f7373e85e5f7ef0209
Nikita Levchuk <code@levch.uk>
2024-02-28 00:39:05 +0100

    feature: add simple game selector
    fix: use one Scanner class with the 'System.in' stream per project
    
    Otherwise, it may lead to unexpected side affects

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,39 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
 
-        Cli.greetings();
+        int gameNumber = getGameNumber(scanner);
+        selectGame(gameNumber, scanner);
+
+        scanner.close();
+    }
+
+    public static int getGameNumber(Scanner scanner) {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String gameNumber = scanner.next();
+
+        return Integer.valueOf(gameNumber);
+    }
+
+    public static void selectGame(int gameNumber, Scanner scanner) {
+        switch (gameNumber) {
+            case 0:
+                System.out.println("Goodbye");
+                break;
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greetings(scanner);
+                break;
+            default:
+                System.out.println("Unknown game number");
+                return;
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,13 +3,9 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-    public static void greetings() {
-        Scanner scanner = new Scanner(System.in);
-
+    public static void greetings(Scanner scanner) {
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-
-        scanner.close();
     }
 }
