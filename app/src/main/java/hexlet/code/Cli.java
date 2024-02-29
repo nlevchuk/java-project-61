3928c471025ded700dfd2056cf678cec3dad1fc9
Nikita Levchuk <code@levch.uk>
2024-02-29 10:27:21 +0100

    fix: revert the Cli class manually
    Keep it in the original state as the step 5 says

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,9 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-    public static void greetings(Scanner scanner) {
+    public static void greetings() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
     }
 }
