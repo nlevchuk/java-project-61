97dbde495fdaf4c17e1f1d032f91185d5ac0d187
Nikita Levchuk <code@levch.uk>
2024-02-29 10:25:54 +0100

    fix: Manage the situation when a user enters incorrect data instead of the game number
    Other changes:
    - Use constants instead of regular numbers (magic numbers)
    - Close the scanner before exiting the main class
    - Place Cli.greetings method content into the main method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,39 +1,59 @@
 package hexlet.code;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class App {
     public static void main(String[] args) {
+        // Constants
+        final int exitNumber = 0;
+        final int greetNumber = 1;
+        final int evenGameNumber = 2;
+
         Scanner scanner = new Scanner(System.in);
 
-        int gameNumber = getGameNumber(scanner);
-        selectGame(gameNumber, scanner);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println(greetNumber + " - Greet");
+        System.out.println(exitNumber + " - Exit");
+        System.out.print("Your choice: ");
+
+        int gameNumber = exitNumber;
+        try {
+            gameNumber = chooseGameNumber(scanner);
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Invalid input. Type one of the numbers above.");
+            scanner.close();
+            return;
+        }
+
+        System.out.println(""); // Add an empty line after the your-choice line
+
+        if (gameNumber == exitNumber) {
+            System.out.println("Goodbye");
+            scanner.close();
+            return;
+        }
+
+        System.out.println("Welcome to the Brain Games!");
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        switch (gameNumber) {
+            case greetNumber:
+                // noop
+                break;
+                break;
+            default:
+                System.out.println("Unknown game");
+        }
 
         scanner.close();
     }
 
-    public static int getGameNumber(Scanner scanner) {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
-        String gameNumber = scanner.next();
-
-        return Integer.valueOf(gameNumber);
-    }
-
-    public static void selectGame(int gameNumber, Scanner scanner) {
-        switch (gameNumber) {
-            case 0:
-                System.out.println("Goodbye");
-                break;
-            case 1:
-                System.out.println("Welcome to the Brain Games!");
-                Cli.greetings(scanner);
-                break;
-            default:
-                System.out.println("Unknown game number");
-                return;
-        }
+    private static int chooseGameNumber(Scanner scanner) throws InputMismatchException {
+        int gameNumber = scanner.nextInt();
+        return gameNumber;
     }
 }
