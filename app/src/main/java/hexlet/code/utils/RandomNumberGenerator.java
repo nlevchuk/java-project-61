9b57ad09a479e801379e550c640e8df932351a8e
Nikita Levchuk <code@levch.uk>
2024-03-05 01:30:21 +0100

    fix: generate positive random numbers (avoid 0)

--- app/src/main/java/hexlet/code/utils/RandomNumberGenerator.java
+++ app/src/main/java/hexlet/code/utils/RandomNumberGenerator.java
@@ -3,7 +3,7 @@ package hexlet.code.utils;
 public class RandomNumberGenerator {
     public static int generate(int length) {
         int randomNumber = (int) (Math.random() * length);
-        return randomNumber;
+        return randomNumber + 1; // to avoid 0
     }
 
     public static int generate() {
